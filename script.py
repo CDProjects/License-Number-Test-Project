820a7c21e498d99c9860c81113cae3ccf2d2c483
Kiwicase <caseydentprojects@gmail.com>
2023-05-02 14:10:55 +0300

    Added email function

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import smtplib
+from email.mime.text import MIMEText
 
 # Connect to MySQL database
 mydb = mysql.connector.connect(
@@ -23,9 +25,9 @@ table2_data = mycursor.fetchall()
 
 # Get Google Sheets credentials and connect to sheet
 scope = ['https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('YOUR_CREDENTIALS_FILE.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('Keys/license-number-test-92056bb3d670.json', scope)
 client = gspread.authorize(creds)
-sheet = client.open('MySQL Test').sheet1
+sheet = client.open('MySQL Test Table').sheet1
 
 # Get all license numbers from the Google Sheet
 sheet_license_numbers = [row[0] for row in sheet.get_all_values()][1:]
@@ -38,7 +40,21 @@ sheet_license_numbers = set(sheet_license_numbers)
 # Find any license numbers that are not in at least one of the sources
 missing_license_numbers = sheet_license_numbers - (table1_license_numbers | table2_license_numbers)
 
-# Print out the missing license numbers
-print("Missing license numbers:")
-for license_number in missing_license_numbers:
-    print(license_number)
+# Create the message to be sent
+message = MIMEText("Missing license numbers:\n" + "\n".join(missing_license_numbers))
+
+# Set the sender and recipient email addresses
+sender = "emailcaseydent@gmail.com"
+recipient = "emailcaseydent@gmail.com"
+
+# Set the subject of the email
+message['Subject'] = 'Missing License Numbers'
+
+# Set the sender and recipient of the email
+message['From'] = sender
+message['To'] = recipient
+
+# Send the email using SMTP
+smtpObj = smtplib.SMTP('your_smtp_server_address')
+smtpObj.sendmail(sender, recipient, message.as_string())
+smtpObj.quit()
