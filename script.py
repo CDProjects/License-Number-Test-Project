b48a31219f6ec50be2ed1d56661791a9078d8988
Kiwicase <caseydentprojects@gmail.com>
2023-05-30 13:25:34 +0300

    Updated script

--- script.py
+++ script.py
@@ -34,10 +34,7 @@ license_numbers_retired_table = mycursor.fetchall()
 # Prefix the license numbers with 'B'
 prefixed_license_numbers_retired_table = ['B' + str(row[0]) for row in license_numbers_retired_table]
 
-# Compile all license numbers into a single list
-all_license_numbers = prefixed_license_numbers_license_table + prefixed_license_numbers_retired_table
-
-# Get Google Sheets credentials and connect to the sheet
+# Get Google Sheets credentials and connect to sheet
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = service_account.Credentials.from_service_account_file('Keys/Google Sheets API.json', scopes=scope)
 client = gspread.authorize(credentials)
@@ -46,8 +43,22 @@ sheet = client.open('MySQL Test Table').sheet1
 # Get all license numbers from the Google Sheet
 sheet_license_numbers = [row[0] for row in sheet.get_all_values()][1:]
 
-# Compare the license numbers from MySQL with the ones in the Google Sheet
-missing_license_numbers = [license_number for license_number in sheet_license_numbers if license_number not in all_license_numbers]
+# Create sets of the license numbers from each source
+license_numbers_license_table = set(prefixed_license_numbers_license_table)
+license_numbers_retired_table = set(prefixed_license_numbers_retired_table)
+sheet_license_numbers = set(sheet_license_numbers)
+
+# Find missing license numbers in the license_numbers_license_table
+missing_license_numbers_license_table = sheet_license_numbers - license_numbers_license_table
+
+# Find missing license numbers in the retired_license_table
+missing_license_numbers_retired_table = sheet_license_numbers - license_numbers_retired_table
+
+# Find missing license numbers in the Google Sheet but present in the compiled list
+missing_license_numbers_in_sheet = license_numbers_license_table.union(license_numbers_retired_table) - sheet_license_numbers
+
+# Combine all missing license numbers
+missing_license_numbers = missing_license_numbers_license_table.union(missing_license_numbers_retired_table, missing_license_numbers_in_sheet)
 
 # Create the message to be sent
 message = MIMEText("Missing license numbers:\n" + "\n".join(missing_license_numbers))
@@ -72,8 +83,10 @@ with smtplib.SMTP('smtp.gmail.com', 587, timeout=120) as smtpObj:
         print(f"SMTP login error: {e}")
         exit(1)
 
-    # Send the email
-    smtpObj.sendmail(sender, recipient, message.as_string())
+    # Send the email if there are missing license numbers
+    if missing_license_numbers:
+        # Send the email
+        smtpObj.sendmail(sender, recipient, message.as_string())
 
-    # Close the connection to the SMTP server
-    smtpObj.quit()
+# Close the connection to the SMTP server
+smtpObj.quit()
